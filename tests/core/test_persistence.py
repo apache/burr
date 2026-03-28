006278fa99a291f01a4ab8527b39809ddde2058e
André Ahlert <andre@aex.partners>
2026-03-28 15:40:39 -0300

    style: reformat with black 23.11.0 (project version)

--- tests/core/test_persistence.py
+++ tests/core/test_persistence.py
@@ -53,9 +53,7 @@ def test_persistence_initialization_creates_table(initializing_persistence):
 def test_persistence_saves_and_loads_state(persistence):
     if hasattr(persistence, "initialize"):
         persistence.initialize()
-    persistence.save(
-        "partition_key", "app_id", 1, "position", State({"key": "value"}), "status"
-    )
+    persistence.save("partition_key", "app_id", 1, "position", State({"key": "value"}), "status")
     loaded_state = persistence.load("partition_key", "app_id")
     assert loaded_state["state"] == State({"key": "value"})
 
@@ -70,12 +68,8 @@ def test_persistence_returns_none_when_no_state(persistence):
 def test_persistence_lists_app_ids(persistence):
     if hasattr(persistence, "initialize"):
         persistence.initialize()
-    persistence.save(
-        "partition_key", "app_id1", 1, "position", State({"key": "value"}), "status"
-    )
-    persistence.save(
-        "partition_key", "app_id2", 1, "position", State({"key": "value"}), "status"
-    )
+    persistence.save("partition_key", "app_id1", 1, "position", State({"key": "value"}), "status")
+    persistence.save("partition_key", "app_id2", 1, "position", State({"key": "value"}), "status")
     app_ids = persistence.list_app_ids("partition_key")
     assert set(app_ids) == set(["app_id1", "app_id2"])
 
@@ -155,9 +149,7 @@ def test_sqlite_persister_list_app_ids_without_initialize_raises_runtime_error()
         ),
     ],
 )
-def test_persister_methods_none_partition_key(
-    persistence, method_name: str, kwargs: dict
-):
+def test_persister_methods_none_partition_key(persistence, method_name: str, kwargs: dict):
     if hasattr(persistence, "initialize"):
         persistence.initialize()
     method = getattr(persistence, method_name)
@@ -254,9 +246,7 @@ async def test_async_persister_methods_none_partition_key(
 async def test_AsyncSQLitePersister_from_values():
     await asyncio.sleep(0.00001)
     connection = await aiosqlite.connect(":memory:")
-    sqlite_persister_init = AsyncSQLitePersister(
-        connection=connection, table_name="test_table"
-    )
+    sqlite_persister_init = AsyncSQLitePersister(connection=connection, table_name="test_table")
     sqlite_persister_from_values = await AsyncSQLitePersister.from_values(
         db_path=":memory:", table_name="test_table"
     )
@@ -310,9 +300,7 @@ async def test_asyncsqlite_persistence_is_initialized_true_new_connection(tmp_pa
     db_path = tmp_path / "test.db"
     p = await AsyncSQLitePersister.from_values(db_path=db_path, table_name="test_table")
     await p.initialize()
-    p2 = await AsyncSQLitePersister.from_values(
-        db_path=db_path, table_name="test_table"
-    )
+    p2 = await AsyncSQLitePersister.from_values(db_path=db_path, table_name="test_table")
     try:
         assert await p.is_initialized()
         assert await p2.is_initialized()
@@ -358,9 +346,7 @@ async def test_async_save_and_load_from_sqlite_persister_end_to_end(tmp_path):
     app = await (
         ApplicationBuilder()
         .with_actions(dummy_input, dummy_response)
-        .with_transitions(
-            ("dummy_input", "dummy_response"), ("dummy_response", "dummy_input")
-        )
+        .with_transitions(("dummy_input", "dummy_response"), ("dummy_response", "dummy_input"))
         .initialize_from(
             initializer=sqlite_persister,
             resume_at_next_action=True,
@@ -390,9 +376,7 @@ async def test_async_save_and_load_from_sqlite_persister_end_to_end(tmp_path):
     new_app = await (
         ApplicationBuilder()
         .with_actions(dummy_input, dummy_response)
-        .with_transitions(
-            ("dummy_input", "dummy_response"), ("dummy_response", "dummy_input")
-        )
+        .with_transitions(("dummy_input", "dummy_response"), ("dummy_response", "dummy_input"))
         .initialize_from(
             initializer=sqlite_persister_2,
             resume_at_next_action=True,
